68bd1b38e74a236fe12c042251d463f9d0c92d4f
Max David <me@maxdavid.com>
2013-05-07 01:43:00 -0700

    Add a counter for the number of checks made.
    Count the number of checks made in the loop. Added an ensure section
    print some stats of the watch session to the user.

--- lib/scorebutt.rb
+++ lib/scorebutt.rb
@@ -17,7 +17,9 @@ module Scorebutt
       # Watcher.new should return an array of procs that test each service for a given host
       watcher_blocks = watchees.get_blocks.flatten!
       puts "Got #{watcher_blocks.size} blocks"
-      loop do
+
+      counter=0 and loop do
+        counter+=1
         watchees.get_blocks.each do |watcher|
           # Watcher is a proc, so run it
           puts watcher.call
@@ -28,6 +30,9 @@ module Scorebutt
     
     rescue SystemExit, Interrupt  # From SIGINT
       puts "\nInterrupt signal recieved"
+    
+    ensure  # Print some stats on program exit
+      puts "#{counter} checks made"
     end
 
   end
